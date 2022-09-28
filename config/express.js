63dc3c7b691fa32efb53486d9645a85f4f8b0945
marcosmgdev <110902924+marcosmgdev@users.noreply.github.com>
2022-09-28 09:54:42 -0300

    cors

--- config/express.js
+++ config/express.js
@@ -1,21 +1,17 @@
 const express = require("express");
 const config = require("config");
 const consign = require("consign");
+const cors = require("cors");
 
 module.exports = () => {
   const app = express();
 
-  // SETANDO VARIÁVEIS DA APLICAÇÃO
   app.set("port", process.env.PORT || config.get("server.port"));
 
-  // MIDDLEWARES
-  app.use(express.json());
+   app.use(express.json());
+   app.use(cors()); 
 
-  consign({ cwd: "api" })
-    .then("data")
-    .then("controllers")
-    .then("routes")
-    .into(app);
+  consign().then("data").then("controllers").then("routes").into(app);
 
   return app;
 };
